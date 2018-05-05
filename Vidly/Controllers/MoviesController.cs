43cae0a8a61e9c10eb1b6660c106462ec4df2ba5
Unknown <jack.yujia.yuan@gmail.com>
2018-05-05 22:25:24 +0800

    Setup views of Customers and Movies
    Display the list of customers and movies in their own  index. WIth Dummy List data.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -60,20 +60,33 @@ namespace Vidly.Controllers
         }
 
         //movies
-        public ActionResult Index(int? pageIndex, string sortBy)
+        //public ActionResult Index(int? pageIndex, string sortBy)
+        public ViewResult Index()
         {
-            if (!pageIndex.HasValue)
-            {
-                pageIndex = 1;
-            }
+            //if (!pageIndex.HasValue)
+            //{
+            //    pageIndex = 1;
+            //}
 
-            if (String.IsNullOrWhiteSpace(sortBy))
-            {
-                sortBy = "Name";
-            }
+            //if (String.IsNullOrWhiteSpace(sortBy))
+            //{
+            //    sortBy = "Name";
+            //}
 
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+            //return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+            var movies = GetMovies();
+            return View(movies);
         }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie {Id = 1, Name = "Shrek"},
+                new Movie {Id = 2, Name = "Wall-e"},
+            };
+        }
+
         [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}|\\d{1}):range(1,12)}")]
         public ActionResult ByReleaseDate(int year, int month)
         {
