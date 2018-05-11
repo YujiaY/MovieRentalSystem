153ce4a29d9f984699ee78fc3ba472330af1bf05
Unknown <jack.yujia.yuan@gmail.com>
2018-05-11 17:29:09 +0800

    Create Customer and MembershipType Tables In DB
    Discarded hard-coded customer data.

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,27 +10,39 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
-        // GET: Customer
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         public ViewResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 
             return View(customers);
         }
 
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer {Id = 1, Name = "John Snow"},
-                new Customer {Id = 2, Name = "Tyrion Lannister"},
+        //private IEnumerable<Customer> GetCustomers()
+        //{
 
-            };
-        }
+        //    return new List<Customer>
+        //    {
+        //        new Customer {Id = 1, Name = "John Snow"},
+        //        new Customer {Id = 2, Name = "Tyrion Lannister"},
+
+        //    };
+        //}
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
             {
